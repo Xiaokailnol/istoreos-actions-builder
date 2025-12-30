3f2e01adb7c3c8b14fc7cfb05fb4f2906bd54704
Xiaokailnol <2519840456@qq.com>
2025-12-30 23:02:35 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -7,3 +7,6 @@ OTA_URL="https://api.kejizero.xyz/openwrt/ota.json"
 
 uci set ota.config.api_url="$OTA_URL"
 uci commit ota
+
+sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
