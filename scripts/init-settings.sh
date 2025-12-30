cdb2a7c550311a01451a261663fa18a6da037b36
Xiaokailnol <2519840456@qq.com>
2025-12-30 19:44:57 +0800

    Update init-settings.sh

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -3,7 +3,7 @@
 . /etc/os-release
 . /lib/functions/uci-defaults.sh
 
-OTA_URL="https://gh-proxy.kejizero.xyz/https://api.kejizero.xyz/openwrt/ota.json"
+OTA_URL="https://api.kejizero.xyz/openwrt/ota.json"
 
 uci set ota.config.api_url="$OTA_URL"
 uci commit ota
