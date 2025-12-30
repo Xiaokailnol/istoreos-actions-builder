4df221c1063738d5b972cf5f8845333f98d5c4ca
Xiaokailnol <2519840456@qq.com>
2025-12-30 19:25:17 +0800

    Create init-settings.sh

---
+++ scripts/init-settings.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+. /etc/os-release
+. /lib/functions/uci-defaults.sh
+
+OTA_URL="https://gh-proxy.kejizero.xyz/https://api.kejizero.xyz/openwrt/ota.json"
+
+uci set ota.config.api_url="$OTA_URL"
+uci commit ota
